b731ff02c1e75d545f7e66dac7c58d8679d84729
sirGoulis14 <t.goulianos@gmail.com>
2024-06-23 20:26:24 +0300

    allowed hosts and typo

--- finalDjango2024/settings.py
+++ finalDjango2024/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-#np+sq&jt-*=oq85bywa&2bn(gcx(+lltpye2ufyp=$%g8&6*-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Thanos14.pythonanywhere.com']
 
 # Application definition
 
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
